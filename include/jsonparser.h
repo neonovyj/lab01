241ac49dba2d6a02594f4d734e78664d0137ddb5
neonovyj <ikhonyak@gmail.com>
2020-11-22 23:13:15 +0300

    CML.txt done

--- include/jsonparser.h
+++ include/jsonparser.h
@@ -5,21 +5,6 @@
 #ifndef LAB_01_PARSER_JSONPARSER_H
 #define LAB_01_PARSER_JSONPARSER_H
 
-#endif  // LAB_01_PARSER_JSONPARSER_H
 
-#include <string>
-#include <iomanip>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <nlohmann/json.hpp>
-#include <any>
-#include <vector>
-#include <Student.h>
 
-using namespace std;
-using json = nlohmann::json;
-
-class jsonparser {
-
-};
+#endif // INCLUDE_JSONPARSER_HPP_
---
+++ tests/jsonparser_test.cpp
@@ -0,0 +1,7 @@
+//
+// Created by ivan on 22.11.2020.
+//
+
+#include <gtest/gtest.h>
+#include <jsonparser.h>
+
